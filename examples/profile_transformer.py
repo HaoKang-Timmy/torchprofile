cba8c00f7b5d9a6cc0adadab3dce5371793152da
Beta Cat <466904389@qq.com>
2023-06-30 21:31:02 +0800

    from list to panda dataframe

--- examples/profile_transformer.py
+++ examples/profile_transformer.py
@@ -2,11 +2,12 @@
 Author: Beta Cat 466904389@qq.com
 Date: 2023-06-25 20:24:22
 LastEditors: Beta Cat 466904389@qq.com
-LastEditTime: 2023-06-30 17:31:06
+LastEditTime: 2023-06-30 20:28:25
 FilePath: /GT2023/torchprofile/examples/profile_transformer.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import torch
+from torch.nn.modules.transformer import Transformer
 from torch.nn.modules.transformer import Transformer
 from torchprofile import profile_macs
 
@@ -23,8 +24,9 @@ if __name__ == '__main__':
     # macs = profile_macs(model, inputs)
     # print('transformer: {:.4g} G'.format(macs / 1e9))
     df,op_df = profile_macs(model, inputs,reduction=None)
-    for item in op_df:
-        print(item)
+    print(op_df)
+    # for item in op_df:
+    #     print(item)
     # for key,value in macs.items():
     #     print("key:   ",key)
     #     print("value:   ",value)
--- torchprofile/profile.py
+++ torchprofile/profile.py
@@ -1,15 +1,25 @@
+'''
+Author: Beta Cat 466904389@qq.com
+Date: 2023-06-25 20:31:12
+LastEditors: Beta Cat 466904389@qq.com
+LastEditTime: 2023-06-30 20:26:50
+FilePath: /torchprofile/examples/torchprofile/profile.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import warnings
 
 from .handlers import handlers
 from .utils.trace import trace
-
+import pandas as pd
+import numpy as np
 __all__ = ['profile_macs']
 
 
 def profile_macs(model, args=(), kwargs=None, reduction=sum):
     results = dict()
-    op_result_df = []
+    op_result_list = []
     graph = trace(model, args, kwargs)
+    flag = 0
     for i,node in enumerate(graph.nodes):
         for operators, func in handlers:
             if isinstance(operators, str):
@@ -19,11 +29,20 @@ def profile_macs(model, args=(), kwargs=None, reduction=sum):
                     # op_result = dict()
                     # op_result[node.operator] = 
                     results[node] = func(node)
-                    op_result_df.append({node.operator:results[node]})
+                    op_result = {"Op_name" : node.operator,
+                                "Macs" : results[node],
+                                "Dimention": node.outputs[-1].shape
+                                    }
+                    
+                    if flag == 0:
+                        column = op_result.keys()
+                        flag = 1
+                    op_result_list.append([op_result[c] for c in column])
                 break
         else:
             warnings.warn('No handlers found: "{}". Skipped.'.format(
                 node.operator))
+    op_result_df = pd.DataFrame(np.array(op_result_list,dtype=object), columns=column, dtype=object)
     if reduction is not None:
         return reduction(results.values())
     else:
